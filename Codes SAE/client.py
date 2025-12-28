2856c88c22873248983a26cbe0ddc7cdb55117cb
Quentin <128997350+Ripley230@users.noreply.github.com>
2025-12-28 20:20:32 +0100

    et

--- Codes SAE/client.py
+++ Codes SAE/client.py
@@ -8,11 +8,9 @@ from PyQt5.QtCore import pyqtSignal
 import crypto
 import socket
 
-# CONFIGURATION : IP de la VM Annuaire (Master)
-# Mettre l'IP du PC qui fait tourner l'annuaire (directory_node.py)
-# Si tout est sur le même PC, laisser 127.0.0.1
+# Si  sur le même PC, laisser 127.0.0.1
 IP_ANNUAIRE = '10.0.2.10'
-# ---------------------
+
 
 class ClientWindow(QMainWindow):
     signal_log = pyqtSignal(str)
@@ -27,12 +25,10 @@ class ClientWindow(QMainWindow):
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
         
-        # --- Partie Réception ---
         self.mon_port = random.randint(9000, 9999)
         self.label_port = QLabel(f"<b>Mon Port (pour recevoir) : {self.mon_port}</b>")
         self.layout.addWidget(self.label_port)
         
-        # --- Partie Envoi ---
         self.layout.addWidget(QLabel("--- Envoi de Message ---"))
         
         self.input_dest_ip = QLineEdit("127.0.0.1")
@@ -57,7 +53,6 @@ class ClientWindow(QMainWindow):
         self.btn_send.clicked.connect(self.envoyer)
         self.layout.addWidget(self.btn_send)
         
-        # --- Logs ---
         self.layout.addWidget(QLabel("--- Logs / Messages Reçus ---"))
         self.logs = QTextEdit()
         self.logs.setReadOnly(True)
@@ -66,7 +61,6 @@ class ClientWindow(QMainWindow):
         self.routeurs = []
         self.signal_log.connect(self.ecrire_log)
         
-        # Démarrer l'écoute
         threading.Thread(target=self.ecouter_messages, daemon=True).start()
 
     def log(self, msg):
@@ -91,7 +85,6 @@ class ClientWindow(QMainWindow):
 
     def gerer_reception(self, client):
         try:
-            # On reçoit un message (qui vient du dernier routeur)
             msg = client.recv(10240).decode('utf-8')
             self.log(f">>> MESSAGE REÇU : {msg}")
             client.close()
@@ -136,43 +129,31 @@ class ClientWindow(QMainWindow):
             return
             
         dest_port = int(dest_port_str)
-        
-        # 1. Récupérer la liste des routeurs
+
         if not self.recuperer_routeurs(): return
         
         if len(self.routeurs) < nb_sauts:
             self.log(f"Erreur : Pas assez de routeurs disponibles ({len(self.routeurs)}) pour {nb_sauts} sauts.")
             return
             
-        # 2. Choisir le chemin (aléatoire)
         chemin = random.sample(self.routeurs, nb_sauts)
         ports_chemin = " -> ".join([str(r['port']) for r in chemin])
         self.log(f"Chemin choisi ({nb_sauts} sauts) : {ports_chemin} -> {dest_port}")
         
-        # 3. Construire l'oignon (Chiffrement en couches)
-        # On part de la fin (le destinataire) et on remonte vers le début.
-        # Le dernier paquet contient : "IP_DEST|PORT_DEST|MESSAGE_CLAIR"
         
         message_a_transmettre = f"{dest_ip}|{dest_port}|{msg}"
-        
-        # On parcourt le chemin à l'envers
+
         for routeur in reversed(chemin):
-            # On chiffre le message pour ce routeur
+
             message_chiffre = crypto.chiffrer(message_a_transmettre, routeur['clef'])
             
-            # Le message à transmettre au routeur PRECEDENT sera :
-            # "IP_DU_ROUTEUR_ACTUEL|PORT_DU_ROUTEUR_ACTUEL|MESSAGE_CHIFFRE"
             message_a_transmettre = f"{routeur['ip']}|{routeur['port']}|{message_chiffre}"
             
-        # A la fin, 'message_a_transmettre' contient "IP_R1|PORT_R1|CHIFFRE_R1"
-        # Le client doit extraire la partie "CHIFFRE_R1" pour l'envoyer à R1.
-        
         parties = message_a_transmettre.split("|", 2)
         ip_premier_noeud = parties[0]
         port_premier_noeud = int(parties[1])
         payload_final = parties[2]
         
-        # 4. Envoyer
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((ip_premier_noeud, port_premier_noeud))
